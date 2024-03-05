9e0be1505c8a47fc69bf513fd361e980766b73de
Alex Felipe Barbosa <alexfelipebarbosa@yahoo.com.br>
2024-03-04 22:30:02 -0300

    Projeto1

---
+++ Cap07/Projeto1/DSA_Projeto1.py
@@ -0,0 +1,66 @@
+# Alex Felipe Barbosa 
+# 04/03/2024
+
+3# Projeto 1 - Desenvolvimento de Game em Linguagem Python - Versão 1
+
+# Import 
+import random
+from os import system, name
+
+# Função para limpar a tela a cada execução
+def limpa_tela():
+  
+  # Windows
+  if name == 'nt':
+    _= system('cls')
+    
+  # Mac ou Linux
+  else:
+    _= system('clear')
+    
+    
+# Função para receber todo o conteudo do jogo
+def game():
+  
+  limpa_tela()
+  
+  print("\nBem-vindo ao jogo da Forca!")
+  print("Adivinhe a palavra abaixo: \n")
+  
+  # Lista de palavras para o jogo
+  palavras = ['banana', 'abacate','uva','morango', 'laranja', 'mexerica','tangerina','acerola','carambola','ameixa','amora','pitaia','pessego','goiaba']
+  
+  # Escolhe randomicamente uma palavra
+  palavra = random.choice(palavras)
+  
+  # List comprehension
+  letras_descobertas  = ['_' for letra in palavra]
+  
+  # numero de chances  
+  chances = 6
+  
+  # Lista para letras erradas
+  letras_erradas = []
+  
+  # Loop enquanto o número de chances for maior que zero
+  while chances > 0:
+    
+    # Print
+    print(" ".join(letras_descobertas))
+    print("\nChances restantes: ", chances)
+    print("Letras erradas: "," ".join(letras_erradas))
+    
+    # Tentativa
+    tentativa = input("\nDigite uma letra: ").lower()
+    
+    # Condicional
+    if tentativa in palavra:
+      index = 0
+      for letra in palavra:
+        if tentativa == letra:
+          letras_descobertas[index] = letra
+        index += 1
+        
+    else:
+      chances -=1
+      letras_erradas.append(tentativa)
